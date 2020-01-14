cdaac37923633c500668ad2fd4cd2135335411fb
gillemp <guplem@gmail.com>
2020-01-14 22:51:59 +0100

    New shorcuts

---
+++ Unity Essentials/Assets/UnityEssentials/SaveProjectShortcut.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SaveProjectShortcut : MonoBehaviour
+{
+    [MenuItem("File/Save project %&s")]
+    static void FunctionForceSaveProyect()
+    {
+        EditorApplication.ExecuteMenuItem("File/Save Project");
+        Debug.Log("Saved project");
+    }
+}
---
+++ Unity Essentials/Assets/UnityEssentials/SaveSceneAndProjectShortcut.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SaveSceneAndProjectShortcut : MonoBehaviour
+{
+    [MenuItem("File/Save Scene And Project %#&s")]
+    static void FunctionForceSaveSceneAndProyect()
+    {
+        EditorApplication.ExecuteMenuItem("File/Save");
+        EditorApplication.ExecuteMenuItem("File/Save Project");
+        Debug.Log("Saved scene and project");
+    }
+}

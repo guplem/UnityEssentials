a061c2ef07ac6163b38bad2dcffb2c7e1fc579da
guplem <guplem@gmail.com>
2020-01-16 20:49:50 +0100

    Documentation

--- Unity Essentials/Assets/UnityEssentials/DebugPro.cs
+++ Unity Essentials/Assets/UnityEssentials/DebugPro.cs
@@ -18,8 +18,6 @@ namespace UnityEngine
             else Debug.Log(str);
         }
         
-        
-        
     }
 
 }
--- Unity Essentials/Assets/UnityEssentials/DefaultConfiguration.cs
+++ Unity Essentials/Assets/UnityEssentials/DefaultConfiguration.cs
@@ -12,9 +12,11 @@ namespace UnityEssentials
             ConsoleFeatures.Clear();
             Debug.Log("Applying recommended configuration...");
             
+            // Actions performed when the button "Apply recommended configuration" is clicked
             QuickSearch.Install();
             SuppressionOfWarningCS0649.DisableWarning();
         }
+        
         
         [MenuItem("Unity Essentials/Settings/Apply Unity's default configuration")]
         public static void RestoreDefaultConfiguration()
@@ -22,6 +24,7 @@ namespace UnityEssentials
             ConsoleFeatures.Clear();
             Debug.Log("Applying Unity's default configuration...");
             
+            // Actions performed when the button "Apply Unity's default configuration" is clicked
             QuickSearch.Uninstall();
             SuppressionOfWarningCS0649.EnableWarning();
         }

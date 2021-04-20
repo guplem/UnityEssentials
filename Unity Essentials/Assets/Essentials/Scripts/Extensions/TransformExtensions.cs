15dc2e6b797fa9e9ad15ebe88f7a647e008b848c
guplem <guplem@gmail.com>
2021-04-20 18:38:39 +0200

    Cleanup

--- Unity Essentials/Assets/Essentials/Scripts/Extensions/TransformExtensions.cs
+++ Unity Essentials/Assets/Essentials/Scripts/Extensions/TransformExtensions.cs
@@ -1,8 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
-using Quaternion = UnityEngine.Quaternion;
-using Vector3 = UnityEngine.Vector3;
 
 namespace UnityEngine
 {
